cec0a0ad852d64b45f6a5017ae1a91f2dd1fd50a
Ridho Azkia Azhar <ridhoibey@gmail.com>
2025-11-23 00:17:11 +0700

    add qr handle

--- index.js
+++ index.js
@@ -53,19 +53,16 @@ const client = new Client({
   puppeteer: {
     headless: true,
     args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--unhandled-rejections=strict",
-      "--disable-extensions",
-      "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process", // <- this one doesn't works in Windows
-      "--disable-gpu",
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu',
     ],
-    // session: sessionConfig
   },
+  webVersionCache: {
+    type: "remote",
+    remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html"
+  }
 });
 
 const saveLastMessage = async (phone, messages) => {
